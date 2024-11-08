4595791aac5deb9dee0e316f58db3f9a78ad0aea
vinicius <vmm_7@hotmail.com>
2024-11-07 21:38:41 -0300

    Add: lucide-react

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,16 @@
+import { ChevronRightIcon } from "lucide-react";
+
 function Tasks({ tasks }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map(task => (
-        <li className="bg-slate-400 text-white p-2 rounded-md" key={task.id}>
-          {task.title}
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-400 text-white p-2 rounded-md w-full text-left">
+            {task.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
         </li>
       ))}
     </ul>
